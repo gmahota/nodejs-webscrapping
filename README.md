8e7d74456288daf0ced824a274e484e9f513545b
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-02-13 23:28:04 +0200

    instagram web scrapping

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodejs-webscrapping
+
+## Run 1
+yarn start
+## Run 2
+
+npx lite-server 
---
+++ index.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.instagram.com/legiaodosherois');
+  await page.screenshot({ path: 'example.png' });
+
+  const imgList = await page.evaluate(()=>{
+    
+    // Vamos pegar todas as imagens que estão na parte de posts
+    const nodeList = document.querySelectorAll('article img')
+
+    // Transformar o NodeList em array
+    const imgArray = [...nodeList]
+
+    //Transformar os nodes (elementos html) em objectos JS
+    const list = imgArray.map(img =>({
+        src:img.src
+    }))
+
+    return list
+  })
+
+  fs.writeFile('instagram.json', JSON.stringify(imgList,null,2),err=>{
+      if(err) throw new Error('someting is wrong')
+
+      console.log('well done')
+  } )
+
+  await browser.close();
+})();
